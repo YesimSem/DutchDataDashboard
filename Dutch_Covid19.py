025f68f8ab8fe5afeda480257b2017aefce49b24
Yesim Semchenko <46399528+YesimSem@users.noreply.github.com>
2023-08-11 21:28:36 +0200

    Update Dutch_Covid19.py

--- Dutch_Covid19.py
+++ Dutch_Covid19.py
@@ -15,18 +15,29 @@ import streamlit as st
 df = pd.read_csv('DutchData_Dashboard.csv')
 df = df.groupby('Province')['Total_reported'].mean()
 df = df.reset_index()
-fig = plt.figure(figsize = (19, 10))
 
-plt.bar(df['Province'], df['Total_reported'], color = 'maroon')
-plt.xlabel('Province')
-plt.ylabel('Reported COVID-19 cases')
-plt.title('Provinces in the Netherlands \ Number of Reported COVID-19 cases')
 pro = df['Province'].unique().tolist()
 province_list = st.multiselect('Choose Province:',options = pro)
 
 
 #displaying the selected options
 
-st.write('You have selected:', province_list)
+st.write(province_list)
+if not province_list:
+    filtered_data = df.copy()
+else:
+    filtered_data = df.loc[df["Province"].isin(province_list)]
+
+st.dataframe(filtered_data, width=1000, height=500)
+
+# from this point on, only the filtered data is used 
+
+fig = plt.figure(figsize = (19, 10))
+
+plt.bar(filtered_data['Province'], filtered_data['Total_reported'], color = 'maroon')
+plt.xlabel('Province')
+plt.ylabel('Reported COVID-19 cases')
+plt.title('Provinces in the Netherlands \ Number of Reported COVID-19 cases')
+
 st.pyplot(fig)
 
