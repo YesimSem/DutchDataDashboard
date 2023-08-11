0da3ae6ac827a8a1304abc89752f8ee3ea767690
Yesim Semchenko <46399528+YesimSem@users.noreply.github.com>
2023-08-11 22:07:28 +0200

    Update Dutch_Covid19.py

--- Dutch_Covid19.py
+++ Dutch_Covid19.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
-
-
-# In[29]:
 
 
 df = pd.read_csv('DutchData_Dashboard.csv')
