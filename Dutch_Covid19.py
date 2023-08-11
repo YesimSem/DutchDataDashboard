4e0937dd3632f1f8669cf5647bb76c403104fe9a
Yesim Semchenko <46399528+YesimSem@users.noreply.github.com>
2023-08-11 21:37:53 +0200

    Update Dutch_Covid19.py

--- Dutch_Covid19.py
+++ Dutch_Covid19.py
@@ -19,7 +19,6 @@ df = df.reset_index()
 pro = df['Province'].unique().tolist()
 province_list = st.multiselect('Choose Province:',options = pro)
 
-
 #displaying the selected options
 
 st.write(province_list)
@@ -28,15 +27,15 @@ if not province_list:
 else:
     filtered_data = df.loc[df["Province"].isin(province_list)]
 
-# st.dataframe(filtered_data, width=1000, height=500)
+# st.dataframe(filtered_data, width=1000, height=500) if you want to see the dataframe just add this.
 
-# from this point on, only the filtered data is used 
+# Only the filtered data is used below.
 
 fig = plt.figure(figsize = (19, 10))
 
 plt.bar(filtered_data['Province'], filtered_data['Total_reported'], color = 'maroon')
 plt.xlabel('Province')
-plt.ylabel('Reported COVID-19 cases')
+plt.ylabel('Average count of COVID-19 cases')
 plt.title('Provinces in the Netherlands \ Average Count of the Reported COVID-19 cases')
 
 st.pyplot(fig)
